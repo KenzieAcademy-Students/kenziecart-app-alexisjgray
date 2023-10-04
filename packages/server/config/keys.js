a44d267617806691c3136a0bfdcbaa659d5c07af
alexis gray <alexisjgray@yahoo.com>
2023-10-04 12:38:26 -0700

    finished

--- packages/server/config/keys.js
+++ packages/server/config/keys.js
@@ -1,14 +1,16 @@
 module.exports = {
   app: {
-    name: 'Mern Social Media',
-    apiEndpoint: process.env.API_URL ? `${process.env.API_URL}` : 'api',
+    name: "Mern Social Media",
+    apiEndpoint: process.env.API_URL ? `${process.env.API_URL}` : "api",
   },
   database: {
     // after the pipes will be your connection string for MongoDB Atlas which you can get from pressing connect on your collection and then clicking connect to application
-    url: process.env.MONGODB_URI || `mongodb+srv://<username>:<password>@cluster0.<cluster_key>.mongodb.net/kenziecart?retryWrites=true&w=majority`,
+    url:
+      process.env.MONGODB_URI ||
+      `mongodb+srv://alexisjgray:AZechXh9LraTCTIb@cluster0.yxlpnnj.mongodb.net/?retryWrites=true`,
   },
   jwt: {
-    secret: process.env.JWT_SECRET || 'jwt-secret',
-    tokenLife: '7d',
+    secret: process.env.JWT_SECRET || "jwt-secret",
+    tokenLife: "7d",
   },
-}
+};
